b7cc2000e2cd524720ae8f0fe4b8a5cfee412930
angelaaaliii <angelali2021@gmail.com>
2024-05-10 14:18:06 -0400

    border

--- my-app/src/fun/FunCrochet.js
+++ my-app/src/fun/FunCrochet.js
@@ -3,8 +3,6 @@ import crocheting_pic from './imgs/crocheting.png';
 export default function FunCrochet() {
 
   return (
-    // <div id="case-study-item" onClick={handleClick}>
-
     <div id="crochet"> 
       <img id ="crocheting-img" alt="Picture of Angela crocheting 3 birbs and pictures of a cactus birb, orange cat birb, oddish and pokeball birbs, and a graduation birb" 
       src={crocheting_pic}></img>
